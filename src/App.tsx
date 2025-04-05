56ad9eb1d2c522377dc121d051d3874ea5da1826
Emmanuel Dwamena <emmergraix@gmail.com>
2025-04-05 11:59:56 +0000

    Update SplashCursor component with fluid animation effect

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,21 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
-            <SplashCursor color="rgba(215, 181, 80, 0.5)" size={100} duration={1000} />
+            <SplashCursor
+              SIM_RESOLUTION={128}
+              DYE_RESOLUTION={1440}
+              DENSITY_DISSIPATION={3.5}
+              VELOCITY_DISSIPATION={2}
+              PRESSURE={0.1}
+              PRESSURE_ITERATIONS={20}
+              CURL={3}
+              SPLAT_RADIUS={0.2}
+              SPLAT_FORCE={6000}
+              SHADING={true}
+              COLOR_UPDATE_SPEED={10}
+              BACK_COLOR={{ r: 0.5, g: 0, b: 0 }}
+              TRANSPARENT={true}
+            />
             <ScrollToTop />
             <Routes>
               <Route path="/" element={<Index />} />
